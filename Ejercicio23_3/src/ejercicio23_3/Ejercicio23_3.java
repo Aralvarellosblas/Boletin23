4ad94e272fdd4d62188f86324514ae1a058f30df
arturo <arturo@ArturoMSI>
2018-04-11 09:24:26 +0200

    Clase principal

---
+++ Ejercicio23_3/src/ejercicio23_3/Ejercicio23_3.java
@@ -0,0 +1,16 @@
+package ejercicio23_3;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio23_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana v=new Ventana();
+    }
+
+}
